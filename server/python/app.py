35589bafdec1e6c2c8ced92306efb612bae03e3e
Moses Mugisha <mossplix@gmail.com>
2018-10-26 22:43:43 +0300

    app endpoints

---
+++ server/python/app.py
@@ -0,0 +1,76 @@
+import os
+from flask import Flask, render_template, jsonify, request, send_from_directory
+app = Flask(__name__)
+
+static_dir = f'{os.path.abspath(os.path.join(__file__ ,"../../.."))}/public'
+app = Flask(__name__, static_folder=static_dir)
+
+
+
+@app.route('/')
+def home():
+    return send_from_directory(static_dir, 'index.html')
+
+
+# Serve config set up in .env
+@app.route('/config')
+def get_config():
+    return jsonify({
+        'auth_key': os.getenv('AUTH_KEY_KEY'),
+        'currency': 'eur'
+    })
+
+
+
+
+@app.route('/javascripts/<path:path>', methods=['GET'])
+def serve_js(path):
+    return send_from_directory(f'{static_dir}/javascripts', path)
+
+
+@app.route('/stylesheets/<path:path>', methods=['GET'])
+def serve_css(path):
+    return send_from_directory(f'{static_dir}/stylesheets', path)
+
+
+@app.route('/images/<path:path>', methods=['GET'])
+def serve_image(path):
+    return send_from_directory(f'{static_dir}/images', path)
+
+@app.route('/products', methods=['GET'])
+def get_products():
+    products = []
+    return jsonify(products)
+
+
+@app.route('/products/<string:product_id>', methods=['GET'])
+def retrieve_product(product_id):
+    pdt={}
+    return jsonify(pdt)
+
+
+@app.route('/orders', methods=['POST'])
+def make_order():
+    # Creates a new Order with items that the user selected.
+    
+    return jsonify({'order': {}})
+  
+
+@app.route('/orders/<string:order_id>/pay', methods=['POST'])
+def pay_order(order_id):
+    response={}
+    order={}
+   
+    
+
+    return jsonify({'order': order, 'source': {}})
+
+
+@app.route('/payment_gateway_callback', methods=['POST'])
+def callback_received():
+    
+
+    return jsonify({'status': 'success'})
+
+
+
