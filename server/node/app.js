ae54040eb31cb9dc9440a5b9a58db7ea078298aa
nico@yoursole.com <nicoflojo.developer@gmail.com>
2018-10-31 14:11:26 +0300

    refactors GET adds Real Request

--- server/node/app.js
+++ server/node/app.js
@@ -16,15 +16,32 @@
 
 // GET Request Framework
 const https = require('https');
-
 https.get('https://encrypted.google.com/', (res) => {
   console.log('statusCode:', res.statusCode);
   console.log('headers:', res.headers);
-
   res.on('data', (d) => {
     process.stdout.write(d);
   });
-
 }).on('error', (e) => {
   console.error(e);
+});
+
+// Real Data Example (GoogleMaps)
+const https = require("https");
+const url =
+  "https://maps.googleapis.com/maps/api/geocode/json?address=Florence";
+https.get(url, res => {
+  res.setEncoding("utf8");
+  let body = "";
+  res.on("data", data => {
+    body += data;
+  });
+  res.on("end", () => {
+    body = JSON.parse(body);
+    console.log(
+      `City: ${body.results[0].formatted_address} -`,
+      `Latitude: ${body.results[0].geometry.location.lat} -`,
+      `Longitude: ${body.results[0].geometry.location.lng}`
+    );
+  });
 });
