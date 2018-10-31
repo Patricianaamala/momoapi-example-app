200dedd9c5e0d2d47f9fee536aaba630066abe88
nico@yoursole.com <nicoflojo.developer@gmail.com>
2018-10-31 13:50:04 +0300

    adds app comment structure

---
+++ server/node/app.js
@@ -0,0 +1,14 @@
+// MTN MoMo APi app
+
+
+
+// Routes:
+// Config
+// Serving: Images, CSS, javascripts
+// HTTP:
+// Get Products
+// Retrieve Products
+// Make Order
+// Pay Order
+//
+// Recieving Callbacks
