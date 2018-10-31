14c678963cb36e3c21af81775d1ec1cf40a3bb7b
nico@yoursole.com <nicoflojo.developer@gmail.com>
2018-10-31 14:10:06 +0300

    adds GET framework

--- server/node/app.js
+++ server/node/app.js
@@ -12,3 +12,19 @@
 // Pay Order
 //
 // Recieving Callbacks
+
+
+// GET Request Framework
+const https = require('https');
+
+https.get('https://encrypted.google.com/', (res) => {
+  console.log('statusCode:', res.statusCode);
+  console.log('headers:', res.headers);
+
+  res.on('data', (d) => {
+    process.stdout.write(d);
+  });
+
+}).on('error', (e) => {
+  console.error(e);
+});
